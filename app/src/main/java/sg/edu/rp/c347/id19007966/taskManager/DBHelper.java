69ce702a36e20d2e75b57e070ca8ace7d991b57f
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-08-03 13:39:54 +0800

    task class + db

--- app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/DBHelper.java
@@ -1,4 +1,86 @@
 package sg.edu.rp.c347.id19007966.taskManager;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "taskstore.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DESCRIPTION = "description";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String creationQuery = "CREATE TABLE "
+                + TABLE_TASK
+                + "("
+                + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_DESCRIPTION + " TEXT" + ")";
+        db.execSQL(creationQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        onCreate(db);
+    }
+
+    public long insertTask(String name, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_DESCRIPTION, description);
+
+        long result = db.insert(TABLE_TASK, null, values);
+        db.close();
+
+        return result;
+    }
+
+    public ArrayList<Task> retrieveAllTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] columns = {COLUMN_ID, COLUMN_NAME, COLUMN_DESCRIPTION};
+        Cursor cursor = db.query(TABLE_TASK, columns, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String description = cursor.getString(2);
+
+                Task task = new Task(id, name, description);
+                tasks.add(task);
+            }
+            while(cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+
+    // TODO: @Muhammad, use this toString for your list
+    public ArrayList<String> retrieveAllStrings() {
+        ArrayList<Task> tasks = retrieveAllTasks();
+        ArrayList<String> taskStrings = new ArrayList<>();
+        for (Task task : tasks) {
+            taskStrings.add(task.toString());
+        }
+        return taskStrings;
+    }
+
 }
--- app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/Task.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/Task.java
@@ -1,4 +1,39 @@
 package sg.edu.rp.c347.id19007966.taskManager;
 
-public class Task {
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private int id;
+    private String name, description;
+
+    public Task(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + "\n" + description;
+    }
 }
