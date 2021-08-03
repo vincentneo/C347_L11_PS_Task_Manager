b46c17d56043aaa429ce35f0142b4f800cefab74
fattahyousaini <50009909+fattahyousaini@users.noreply.github.com>
2021-08-03 14:19:06 +0800

    Broadcast Receiver

--- app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/ScheduledNotificationReceiver.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/ScheduledNotificationReceiver.java
@@ -1,8 +1,15 @@
 package sg.edu.rp.c347.id19007966.taskManager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
 
 public class ScheduledNotificationReceiver extends BroadcastReceiver {
 
@@ -10,6 +17,33 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // throw new UnsupportedOperationException("Not yet implemented");
+        String name = intent.getStringExtra("name");
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION. SDK_INT >= Build.VERSION_CODES. O ) {
+            NotificationChannel channel = new
+                    NotificationChannel(" default", "Default Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent i = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0,
+                intent, 0);
+
+        // build notification
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(context, "channel");
+        builder.setContentTitle("Reminder from Task Manager");
+        builder.setContentText(name);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+
+        Notification n = builder.build();
+        notificationManager.notify(0, n);
     }
 }
