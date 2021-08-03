91c8a4d3af2a38ba97aee2775c2a570b4af76a6b
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-08-03 12:36:56 +0800

    Empty files for later use

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/DBHelper.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c347.id19007966.taskManager;
+
+public class DBHelper {
+}
---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/taskManager/ScheduledNotificationReceiver.java
@@ -0,0 +1,15 @@
+package sg.edu.rp.c347.id19007966.taskManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ScheduledNotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
